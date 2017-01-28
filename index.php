7819bf1169c473ec3c24ffda24bfde912eaeb9b4
Claudia De Leon <claulg2701@gmail.com>
2017-01-28 15:01:08 -0500

    initial content for index and css

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+  <title>Claudia De Leon Blank Template</title>
+  </head>
+  <body>
+    <h1>Blank WordPress Template</h1>
+  	<p>This is my WordPress template.</p>
+  </body>
+</html>
