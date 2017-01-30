89c4cf434a945b231201d9dbe2aa4cad28740ae6
Claudia De Leon <claulg2701@gmail.com>
2017-01-29 19:37:29 -0500

    add basic grid layout to index

--- index.php
+++ index.php
@@ -7,7 +7,25 @@
 		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
   </head>
   <body>
-    <h1>Blank WordPress Template</h1>
-  	<p>This is my WordPress template.</p>
+    <div class="container">
+      <header class="row">
+        <div class="twelve columns">
+          <h1>Claudia De Leon Blank WordPress Template</h1>
+          <p>This is my WordPress template.</p>
+        </div>
+      </header>
+      <section class="row">
+        <div class="twelve columns">
+          <h2>Section Content</h2>
+          <p>This is a section content paragraph.</p>
+        </div>
+      </section>
+      <footer class="row">
+        <div class="twelve columns">
+          <h2>This is my footer</h2>
+          <p>This is a footer paragraph.</p>
+        </div>
+      </footer>
+    </div>
   </body>
 </html>
