31f3702ba6548b70eb11d82f1844e8ca11551465
Claudia De Leon <claulg2701@gmail.com>
2017-01-30 13:02:07 -0500

    dinamic load of header and footer

---
+++ footer.php
@@ -0,0 +1,10 @@
+  <!-- BEGINNING OF FOOTER - END SECTION CONTENT -->
+    <footer class="row">
+      <div class="twelve columns">
+        <h2>This is my footer</h2>
+        <p>This is a footer paragraph.</p>
+      </div>
+    </footer>
+  </div> <!--end container class-->
+  <?php wp_footer(); ?>
+</html>
--- index.php
+++ index.php
@@ -1,31 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-  <title>Claudia De Leon Blank Template</title>
-  <!-- Links to our Style.css file -->
-		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
-  </head>
-  <body>
-    <div class="container">
-      <header class="row">
-        <div class="twelve columns">
-          <h1>Claudia De Leon Blank WordPress Template</h1>
-          <p>This is my WordPress template.</p>
-        </div>
-      </header>
-      <section class="row">
-        <div class="twelve columns">
-          <h2>Section Content</h2>
-          <p>This is a section content paragraph.</p>
-        </div>
-      </section>
-      <footer class="row">
-        <div class="twelve columns">
-          <h2>This is my footer</h2>
-          <p>This is a footer paragraph.</p>
-        </div>
-      </footer>
+<?php get_header(); ?>
+  <section class="row">
+    <div class="twelve columns">
+      <h2>Section Content</h2>
+      <p>This is a section content for the body.</p>
     </div>
-  </body>
-</html>
+  </section>
+<?php get_footer(); ?>
