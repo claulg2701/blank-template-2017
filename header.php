5b6ed46127f0deb09201e528f2e0bf7bd7df4e53
Claudia De Leon <claulg2701@gmail.com>
2017-02-08 23:13:23 -0500

    add search functionality

--- header.php
+++ header.php
@@ -9,10 +9,14 @@
 	<body>
 		<div class="container">
 			<header class="row">
-				<div class="twelve columns">
+				<div class="nine columns">
 					<h1><?php bloginfo('name'); ?>
 					<p><?php bloginfo('description'); ?></p>
 				</div>
+        <!-- Add Search Form -->
+      	<div class="three columns">
+      		<?php get_search_form(); ?>
+      	</div>
 			</header>
       <!-- end of header | begin section content -->
       <!-- Menu -->
