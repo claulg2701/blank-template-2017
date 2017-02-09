e829259488349cc0fa6360a1c3d11ac8b35d0a2d
Claudia De Leon <claulg2701@gmail.com>
2017-02-08 21:52:00 -0500

    add sidebar and display it in the page

--- page.php
+++ page.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 	<section class="row">
-		<div class="twelve columns">
+		<div class="nine columns">
       <!-- Begin page PHP-->
 			<?php if (have_posts()) :
-				/* OUR DATA CONTEXT IS DEFINED 	*/
+				/* data context	*/
 				while (have_posts()) : the_post(); ?>
 					<h2><?php the_title(); ?></h2>
 					<?php the_content();
@@ -12,4 +12,9 @@
       <!-- End page PHP -->
 		</div>
 	</section>
+  <!-- Begin sidebar -->
+	<div class="three columns">
+		<?php get_sidebar(); ?>
+	</div>
+  <!-- End sidebar -->
 <?php get_footer(); ?>
