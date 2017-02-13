275931dfabe2dd758e208efdbb890b1cea539ee9
Claudia De Leon <claulg2701@gmail.com>
2017-02-13 15:12:55 -0500

    fixed error displaying search results

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
   <section class="row">
     <div class="twelve columns">
       <!--Begin loop-->
-        <?php
-          if ( have_posts() ) {
-            while ( have_posts() ) {
+        <?php if ( have_posts() ) {
+                while ( have_posts() ) {
                 the_post();?>
-                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-			          <?php
-                if ( has_post_thumbnail() ) {
+                <h3>
+                  <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                </h3>
+			          <?php if ( has_post_thumbnail() ) {
 	                    the_post_thumbnail('thumbnail');
                 }
                the_excerpt(); ?>
